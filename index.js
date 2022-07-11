aac479c28781499a2dda45b492e4cdd29a8088a5
vincadrn <wsamudro@gmail.com>
2022-07-11 18:12:36 +0700

    Initial commit

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
-var express = require('express');
-var cors = require('cors');
 require('dotenv').config()
-
-var app = express();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const multer = require('multer');
+const upload = multer({dest: './uploads'});
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post("/api/fileanalyse", upload.single('upfile'), (req, res) => {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
 });
 
 
@@ -16,5 +25,5 @@ app.get('/', function (req, res) {
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log('App is listening on port ' + port)
 });
